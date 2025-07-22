1e27980e80647a39b5754d92b2b97074fba6cd1a
Bilgesu Barış <bilgesubaris@gmail.com>
2025-07-22 10:43:38 +0300

    Initial Commit

---
+++ staj/stajApp.swift
@@ -0,0 +1,17 @@
+//
+//  stajApp.swift
+//  staj
+//
+//  Created by Bilgesu Barış on 22.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct stajApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stajTests/stajTests.swift
@@ -0,0 +1,17 @@
+//
+//  stajTests.swift
+//  stajTests
+//
+//  Created by Bilgesu Barış on 22.07.2025.
+//
+
+import Testing
+@testable import staj
+
+struct stajTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
